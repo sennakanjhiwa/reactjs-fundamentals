34ef74b2d4af66f17067959af311ef05e87cdafa
Senna Bhany Majid <sennakanjhiwa@gmail.com>
2025-08-06 20:58:01 +0700

    feat: add ability to pass value from child to parent via props

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
 import Article from "../components/Article";
-import posts from "../posts.json";
+import dataPosts from "../posts.json";
+import Search from "../components/Search";
+import { useState } from "react";
 
 function Homepage() {
-    const [search, setSearch] = useState("");
+    const [posts, setPosts] = useState(dataPosts)
 
-    const changeSearch = (event) => {
-        setSearch(event.target.value);
+    const onSearchChange = (value) => {
+        const filteredPosts = dataPosts.filter(item => item.title.includes(value));
+        setPosts(filteredPosts);
     };
     return (
         <>
             <h1>Simple Blogs</h1>
-            <div>
-                Cari artikel : <input onChange={changeSearch} type="text" />{""}
-            </div>
-            <small>Ditemikan 0 data dengan pencarian kata {search}</small>
-            {posts.map(({title, tags, date}, index) => (
-                <Article {...{title, tags, date}} key={index} />
+            <Search onSearchChange={onSearchChange} />
+            {posts.map((props, index) => (
+                <Article {...props} key={index} />
             ))}
         </>
     );
