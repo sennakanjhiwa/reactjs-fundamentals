53f177f7aadcab430c5622cf161ca59967d5b36d
Senna Bhany Majid <sennakanjhiwa@gmail.com>
2025-08-07 09:42:06 +0700

    feat: onclick methods

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,15 +3,24 @@ import { useState } from "react";
 function Search(props) {
     const [search, setSearch] = useState("");
 
-    const onSearchChange = (event) => {
-        setSearch(event.target.value);
-        props.onSearchChange(event.target.value);
+    const onSearchChange = () => {
+        props.onSearchChange(search);
+    };
+
+    const searchKeyDown = (e) => {
+        if ((e.key === "Enter")) {
+            onSearchChange();
+        }
     };
 
     return (
         <>
             <div>
-                Cari artikel : <input onChange={onSearchChange} type="text" />{""}
+                Cari artikel : 
+                <input 
+                onChange={(e) => setSearch(e.target.value)} 
+                onKeyDown={searchKeyDown}
+                type="text" /><button onClick={onSearchChange}>Cari</button>
             </div>
             <small>Ditemikan {props.totalPosts} data dengan pencarian kata {search}</small>
         </>
