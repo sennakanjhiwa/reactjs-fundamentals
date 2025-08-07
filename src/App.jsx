924862550b1a1d362b755de21825f4e9632aa83c
Senna Bhany Majid <sennakanjhiwa@gmail.com>
2025-08-07 08:36:14 +0700

    feat: conditional rendering 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import './App.css';
 import Homepage from './pages/Index';
 
 function App() {
-  const value = false
   return (
     <>
       <div className="card">
-        {value ? "Benar" : "Salah"}
         <Homepage />
       </div>
     </>
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,8 +1,20 @@
+const ArticleStatus = ({isNew}) => {
+    return isNew && <span>--Baru</span>;
+};
+
+const NewArticle = () => {
+    return <span> --Baru</span>;
+};
+
 function Article(props) {
     return (
         <>
             <h2>{props.title}</h2>
-            <small>Date: {props.date}, Tags: {props.tags.join(", ")}</small>
+            <small>
+                Date: {props.date}, Tags: {props.tags.join(", ")}{" "}
+                <ArticleStatus isNew={props.isNew} />
+                {props.isNew && <NewArticle />}
+            </small>
         </>
     );
 }
