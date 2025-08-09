b1d8df75ae0bfda54ef45ca7c0153ffdb4587561
Senna Bhany Majid <sennakanjhiwa@gmail.com>
2025-08-09 13:55:39 +0700

    feat: create about page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Homepage from "../pages/Index";
+import About from "../pages/About";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Homepage />
+        element: <Homepage />,
+    },
+    {
+        path: "/about",
+        element: <About />,
     },
 ]);
